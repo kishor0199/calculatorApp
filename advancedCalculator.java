f178be91b66a94e6d3049d3756c068fc2fe11082
60919_Kishor <kishorjanjal40@gmail.com>
2022-06-21 10:03:37 +0530

    Adding Advance Calculator Program From newfeatures Branch

---
+++ advancedCalculator.java
@@ -0,0 +1,14 @@
+class AdvanceCalculator {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is Advanced Calculator");
+
+    }
+
+    public int min(int a , int b){
+
+        return Math.min(a, b);
+    }
+
+}
