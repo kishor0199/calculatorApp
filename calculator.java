dd7855f2fe3e28184a8e6c038c3d23ba40d7a4ad
60919_Kishor <kishorjanjal40@gmail.com>
2022-06-21 09:38:11 +0530

    First Commit adding Calculator App

---
+++ calculator.java
@@ -0,0 +1,17 @@
+ class Calculator {
+
+    public static int add(int a, int b) {
+
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Addition is :" + add(a, b));
+
+    }
+
+}
